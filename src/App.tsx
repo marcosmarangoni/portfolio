515637a577b24192a7522130c23c1eaa2e07b190
Marcos Marangoni <marangonim@student.douglascollege.ca>
2021-03-30 17:26:51 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     <Router>
       <Header />
       <Switch>
-        <Route path="/portfolio/dijkstra">
+        <Route exact path="/portfolio/dijkstra">
           <Dijkstra />
         </Route>
         <Route path="/portfolio">
